9e86b80fcb7a39c8efc2c85a9188b339c9bd4b29
Chaitanya Tanna <chaitanyatanna3@gmail.com>
2017-04-13 15:11:28 -0700

    formatted CAA and CEA scripts

--- Scripts/Event/CAA;LICENSES!~!~!~.js
+++ Scripts/Event/CAA;LICENSES!~!~!~.js
@@ -1,16 +1,25 @@
-//CAA;LICENSES!~!~!~.js
-//Greg Soter, FutureNet Group, Inc.
-//Deploy with the script code and script title below (all caps)
-//CAA:LICENSES/*/*/*
+/**
+ * Event Name:- Contact Add After
+ * Event Description:- The after event for when a user adds a contact
+ * MasterScript:- ContactAddAfterV3.0.js
+ * Record Type:- CAA;LICENSES!~!~!~.js
+ * 
+ * Greg Soter, FutureNet Group, Inc.
+ * 
+ * TODO: NEED TO add appMatch("") strings to specify which food service record types will trigger this script
+ * 
+ * Formatted By:- Chaitanya Tanna, City of Detroit
+ */
+
 if (matches(currentUserID,"ADMIN")) {
-showDebug = false;
-showMessage= false;
+	showDebug = false;
+	showMessage= false;
 }
 
 //branch("EMSE:SetContactRelationshipToContactType");
 logDebug("Executing EMSE:SetContactRelationshipToContactType")
 if (matches(currentUserID,"ADMIN")) {
-    showDebug = false;
+	showDebug = false;
     showMessage= false;
 }
 
@@ -18,7 +27,7 @@ iCont = null;
 contactArray = new Array();
 contactArray = getContactArray();
 if (contactArray.length > 0) {
-    for (iCont in contactArray){
+    for (iCont in contactArray) {
             //branch("EMSE:SetContactRelationshipToContactTypeLoop");
             logDebug("Executing EMSE:SetContactRelationshipToContactTypeLoop");
             showDebug=3;
--- Scripts/Event/CEA;LICENSES!~!~!~.js
+++ Scripts/Event/CEA;LICENSES!~!~!~.js
@@ -1,12 +1,20 @@
-//CEA;LICENSES!~!~!~.js
-//Greg Soter, FutureNet Group, Inc.
-//Deploy with the script code and script title below (all caps)
-//CEA:LICENSES/*/*/*
+/**
+ * Event Name:- Contact Edit After
+ * Event Description:- The after event for when a user edits a contact
+ * MasterScript:- ContactEditAfterV3.0.js
+ * Record Type:- CEA;LICENSES!~!~!~.js
+ * 
+ * Greg Soter, FutureNet Group, Inc.
+ * 
+ * TODO: NEED TO add appMatch("") strings to specify which food service record types will trigger this script
+ * 
+ * Formatted By:- Chaitanya Tanna, City of Detroit
+ */
+
 if (matches(currentUserID,"ADMIN")) {
     showDebug = false;
     showMessage= false;
 }
-
 //branch("EMSE:SetContactRelationshipToContactType");
 logDebug("Executing EMSE:SetContactRelationshipToContactType")
 if (matches(currentUserID,"ADMIN")) {
@@ -18,7 +26,7 @@ iCont = null;
 contactArray = new Array();
 contactArray = getContactArray();
 if (contactArray.length > 0) {
-    for (iCont in contactArray){
+    for (iCont in contactArray) {
         //branch("EMSE:SetContactRelationshipToContactTypeLoop");
         logDebug("Executing EMSE:SetContactRelationshipToContactTypeLoop");
         showDebug=3;
