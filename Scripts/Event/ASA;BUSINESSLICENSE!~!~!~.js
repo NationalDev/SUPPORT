1612299acde3111e732f9245ab4629f33a711208
NationalDev <nd.michigan@gmail.com>
2017-07-31 14:51:34 -0400

    New

--- Scripts/Event/ASA;BUSINESSLICENSE!~!~!~.js
+++ Scripts/Event/ASA;BUSINESSLICENSE!~!~!~.js
@@ -9,46 +9,45 @@
 //
 ////
 //
-////
-//
-////branch("EMSE:SetContactRelationshipToContactType");
-////logDebug("branch(EMSE:SetContactRelationshipToContactType)")
-////if (matches(currentUserID,"ADMIN")) {
-//    showDebug = true;
-//    showMessage= true;
-////}
-//if capID <> null {
-//var parentRecordID = getParent();
-//
-//copyAddresses(capId,parentRecordID);
-//
-//logDebug("Record ID = " + parentRecordID ); 
-//logDebug("Street No = " + 'Street#' ); 
-//logDebug("Street Name = " + 'Street Name' ); 
-//logDebug("Street Type = " + 'Street Type' ); 
-//logDebug("Address Line 1 = " + 'Address Line 1' ); 
-//logDebug("Address Line 2 = " + 'Address Line 2' ); 
-//logDebug("City = " + City ); 
-//logDebug("Zip = " + Zip ); 
-//
-//}
-//
-//
-//
-////iCont = null;
-////contactArray = new Array();
-////contactArray = getContactArray();
-////if (contactArray.length > 0) {
-////    for (iCont in contactArray){
-////        //branch("EMSE:SetContactRelationshipToContactTypeLoop");
-////        logDebug("branch(EMSE:SetContactRelationshipToContactTypeLoop)");
-////        showDebug=3;
-////        tContact = contactArray[iCont];
-////        aa.print("ContactName: " + tContact["firstName"] + " " + tContact["lastName"] + " " + tContact["contactType"]);
-////        contactSetRelation(tContact["contactSeqNumber"], tContact["contactType"]);
-////    }
-////}
-////
-//////branch("ASIUA:Licenses/*/*/License");
-////logDebug("branch(ASIUA:Licenses/*/*/License)")
-////editAppName(getAppSpecific("LIC Doing Business As (DBA) Name"));
+aa.runScript("APPLICATIONSUBMITAFTER4RENEW");
+aa.cap.updateAccessByACA(capId,"Y");
+//updateFee("LIC_030","LIC_CONTRACTOR_GENERAL","FINAL",1,"Y");
+//branch("EMSE:LicProfLookup:getLicenses");
+var searchCap = capId;
+var tmpId = capId;
+var prjArr = null;
+if (appMatch("*/*/*/License")) {
+    var childArr = getChildren("*/*/*/Application");
+    if(childArr != null) 
+    	searchCap = childArr[0];
+}
+
+capId = tmpId;
+var vRelationType = "R";
+if(appMatch("*/*/*/Renewal")) 
+	vRelationType = "Renewal";
+var prjArrRes = aa.cap.getProjectByChildCapID(searchCap,vRelationType,null);
+if(prjArrRes.getSuccess()) 
+	prjArr = prjArrRes.getOutput();
+if (prjArr != null) {
+    for(prj in prjArr) 
+    	if(appMatch("*/*/*/License",prjArr[prj].getProjectID())) 
+    		licCapId = prjArr[prj].getProjectID();
+}
+
+if (licCapId == null && appMatch("*/*/*/License")) {
+    licCapId = capId;
+    //In the event license has no application;
+}
+if (licCapId == null && appMatch("*/*/*/Renewal")) {
+    licCapId = capId;
+    //In the event license has no application;
+}
+if (licCapId != null) {
+    licCapId = aa.cap.getCapID(licCapId.getID1(),licCapId.getID2(),licCapId.getID3()).getOutput();
+}
+expDate = aa.expiration.getLicensesByCapID(licCapId).getOutput().getB1Expiration().getExpDate();
+now = aa.util.now();
+tempDate = aa.util.formatDate(now, "MM/dd/YYYY");
+today = aa.util.parseDate(tempDate);
+today.after(expDate)?updateFee("LIC_040","LIC_CONTRACTOR_GENERAL","FINAL",1,"Y"):now;
