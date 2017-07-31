be5c02d5b1466028440f712350e26115a802e0d8
NationalDev <nd.michigan@gmail.com>
2017-07-31 14:56:35 -0400

    1ST

--- Scripts/Event/ASA;BUSINESSLICENSE!~!~!RENEWAL.js
+++ Scripts/Event/ASA;BUSINESSLICENSE!~!~!RENEWAL.js
@@ -1,26 +1,52 @@
-/**
- * To run ASA4Renew script when the renew button is selected in AA or when the
- * Renewal link is selected in ACA
- * 
- * Event Name:- Application Submit After
- * Event Description:- After Event for Application Submittal
- * Master Script:- ApplicationSubmitAfterV3.0.js
- * 
- * Record Type:- ASA;BUSINESSLICENSE!~!~!RENEWAL.js
- * 
- * Formatted By:- Chaitanya Tanna, City of Detroit
- */
+//*********************************************************************************************************/
+//	ASA;BUSINESSLICENSE!~!~!RENEWAL.js															       /	
+//																			Iman Sallam @ City of Detroit  /
+//		Deploy with the script code and script title below (all caps)									   /
+//																								           /
+//					ASA:BUSINESSLICENSE/*/*/RENEWAL							7/31/2017							 							
+//																										   /
+//*********************************************************************************************************/
+//ASA:BUSINESSLICENSE/*/*/RENEWAL script
 
-//showDebug = true; showMessage = true;
-//logDebug("next line runs ASA4Renew script in new transaction");
 aa.runScript("APPLICATIONSUBMITAFTER4RENEW");
-//lines added by CIH 03012016
-newLicId = getParentCapID4Renewal();
-var appName = getAppName(newLicId); //lines added by CIH 03012016
-if (newLicId) {
-	editAppName(appName,capId); //copy appName from License record to renewal record
+aa.cap.updateAccessByACA(capId,"Y");
+//updateFee("LIC_030","LIC_CONTRACTOR_GENERAL","FINAL",1,"Y");
+//branch("EMSE:LicProfLookup:getLicenses");
+var searchCap = capId;
+var tmpId = capId;
+var prjArr = null;
+if (appMatch("*/*/*/License")) {
+    var childArr = getChildren("*/*/*/Application");
+    if(childArr != null) 
+    	searchCap = childArr[0];
 }
-//logDebug("ASA4Renew script ran successfully");
-//	aa.cap.updateAccessByACA(capId,"Y");
-//logDebug("updateAccessByACA ran successfully");
 
+capId = tmpId;
+var vRelationType = "R";
+if(appMatch("*/*/*/Renewal")) 
+	vRelationType = "Renewal";
+var prjArrRes = aa.cap.getProjectByChildCapID(searchCap,vRelationType,null);
+if(prjArrRes.getSuccess()) 
+	prjArr = prjArrRes.getOutput();
+if (prjArr != null) {
+    for(prj in prjArr) 
+    	if(appMatch("*/*/*/License",prjArr[prj].getProjectID())) 
+    		licCapId = prjArr[prj].getProjectID();
+}
+
+if (licCapId == null && appMatch("*/*/*/License")) {
+    licCapId = capId;
+    //In the event license has no application;
+}
+if (licCapId == null && appMatch("*/*/*/Renewal")) {
+    licCapId = capId;
+    //In the event license has no application;
+}
+if (licCapId != null) {
+    licCapId = aa.cap.getCapID(licCapId.getID1(),licCapId.getID2(),licCapId.getID3()).getOutput();
+}
+expDate = aa.expiration.getLicensesByCapID(licCapId).getOutput().getB1Expiration().getExpDate();
+now = aa.util.now();
+tempDate = aa.util.formatDate(now, "MM/dd/YYYY");
+today = aa.util.parseDate(tempDate);
+today.after(expDate)?updateFee("LIC_040","LIC_CONTRACTOR_GENERAL","FINAL",1,"Y"):now;
