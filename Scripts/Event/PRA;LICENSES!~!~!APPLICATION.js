fea060c9b257f1bc3a48cdb719d0f7e4c9af1fdd
NationalDev <nd.michigan@gmail.com>
2017-09-01 18:40:28 -0400

    new place

--- Scripts/Event/PRA;LICENSES!~!~!APPLICATION.js
+++ Scripts/Event/PRA;LICENSES!~!~!APPLICATION.js
@@ -10,7 +10,6 @@
 var showDebug = true;
 var showMessage = true;
 
-logDebug("Balance Due = " + balanceDue + "Task Active = "  + isTaskActive("License Issuance") + " Status =" + taskStatus("License Issuance"); 
 
 //if (isTaskStatus == "Request for Corrections") {
 //        sendExternalReviewNotification();   
@@ -49,6 +48,7 @@ if (newLicId) {
         
         logDebug("Balance Due = " + balanceDue + " Record Id " + newLicString); 
                              
+        logDebug("Balance Due = " + balanceDue + "Task Active = "  + isTaskActive("License Issuance") + " Status =" + taskStatus("License Issuance"); 
 
 
 //        var feeArr = loadFees();
