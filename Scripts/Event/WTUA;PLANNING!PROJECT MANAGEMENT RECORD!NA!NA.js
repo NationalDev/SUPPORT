94027c6deb88e7d70934669731846a6044a04b40
NationalDev <nd.michigan@gmail.com>
2017-09-01 16:46:22 -0400

    aa
    aa

--- Scripts/Event/WTUA;PLANNING!PROJECT MANAGEMENT RECORD!NA!NA.js
+++ Scripts/Event/WTUA;PLANNING!PROJECT MANAGEMENT RECORD!NA!NA.js
@@ -1,35 +1,18 @@
 //WTUA:Planning/Project Management Record/NA/NA
+var appName = getAppName(capId);
+logDebug("appName is: " +appName);
 
-if (wfTask.equals("Project Review Intake") && wfStatus.equal("Submit Review Request")) {
-
-	if (AInfo['Construction Plan Review'] == "CHECKED"){
-		createChild("Planning","Plan Review","NA","NA",capName);
-		editTaskSpecific("Project Review Intake","Construction Plan Review","UNCHECKED");
+if (wfTask.equals("Project Review Intake") && wfStatus.equals("No Action")) {
+	closeTask("Plan Review Scheduled","No Action","Closed via Script","Closed via Script");
 	}
-	if (AInfo['Site Plan Review'] == "CHECKED"){
-		createChild("Planning","Site Plan Review","NA","NA",capName);
-		editTaskSpecific("Project Review Intake","Site Plan Review","UNCHECKED");
-	}
-	if (AInfo['Fire Plan Review'] == "CHECKED"){
-		createChild("Planning","Fire Plan Review","NA","NA",capName);
-		editTaskSpecific("Project Review Intake","Fire Plan Review","UNCHECKED");
-	}
-	if (AInfo['Preliminary Plan Review'] == "CHECKED"){
-		var pprCapId = createChild("Planning","Preliminary Project Review","NA","NA",capName);
-		var holdId = capId;
-		capId = pprCapId;
-		updateFee("PPR","PLNPPR_F","FINAL",1,"Y");
-		capId = holdId;
-		
-		editTaskSpecific("Project Review Intake","Preliminary Plan Review","UNCHECKED");
-	}
-	if (AInfo['Zoning Verification'] == "CHECKED"){
-		createChild("Planning","Verification","NA","NA",capName);
-		editTaskSpecific("Project Review Intake","Zoning Verification","UNCHECKED");
-	}
-}
 	
-
+if (wfTask.equals("Plan Review Scheduled") && matches(wfStatus,"Submit CPR","Submit CPR and SPR")) {
+	createChild("Permits","Review","Structural","NA",appName);
+	}
+	
+if (wfTask.equals("Plan Review Scheduled") && matches(wfStatus,"Submit SPR","Submit CPR and SPR")) {
+	createChild("Planning","Site Plan Review","NA","NA",appName);
+	}
 
 	
 	
