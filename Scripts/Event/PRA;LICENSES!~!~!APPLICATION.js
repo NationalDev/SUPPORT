7a3af9a783850502043147e2351faa0324093ad3
NationalDev <nd.michigan@gmail.com>
2017-09-02 09:47:08 -0400

    runreport

--- Scripts/Event/PRA;LICENSES!~!~!APPLICATION.js
+++ Scripts/Event/PRA;LICENSES!~!~!APPLICATION.js
@@ -227,12 +227,12 @@ if (isTaskStatus("License Issuance","issued") && balanceDue <= 0) {
 		}
 		else {    	
     	    	      	var rParams = aa.util.newHashMap();
-    	    	        addParameter(rParams,"Record_ID",String(newLicId.getCustomID()));
+    	    	        addParameter(rParams,"Record_ID",altID);
     	    	        addParameter(rParams,"Module","Licenses");
     	    	        addParameter(rParams,"Fee Description","License");
     	    	        addParameter(rParams,"logo","Xtra4");
     	    	        logDebug("Parameters: " + rParams);
-    	    	        runReport4EmailOrPrint(newLicId,"License",null,rParams,null,null,"Licenses");
+    	    	        runReport4EmailOrPrint(altID,"License",null,rParams,null,null,"Licenses");
 	      	
     	  	}
     	    	      	
