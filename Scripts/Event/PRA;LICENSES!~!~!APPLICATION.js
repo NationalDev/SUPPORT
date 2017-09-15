b1c3dfb252fd2efd1f7e98b98e1d710bc29791e4
NationalDev <nd.michigan@gmail.com>
2017-09-15 11:13:37 -0400

    339-348

--- Scripts/Event/PRA;LICENSES!~!~!APPLICATION.js
+++ Scripts/Event/PRA;LICENSES!~!~!APPLICATION.js
@@ -336,16 +336,16 @@ var showMessage = true;
 //	                isNewLic = true;
 //	          }
 	        
-//	      if (tmpLicObj.valid && licIDString) {
-	          associatedRefContactWithRefLicProf(licObj.refLicModel.getLicSeqNbr(), aa.getServiceProviderCode(),currentUserID);
+////	      if (tmpLicObj.valid && licIDString) {
+//	          associatedRefContactWithRefLicProf(licObj.refLicModel.getLicSeqNbr(), aa.getServiceProviderCode(),currentUserID);
+////	          }
+//	
+//	      var mycap = aa.cap.getCap(tmpLicObj).getOutput();
+//	      if (tmpLicObj.valid && mycap.getCapModel().getCreatedByACA() == 'Y') {
+//	          associatedLicensedProfessionalWithPublicUser(licObj.refLicModel.getLicSeqNbr(), mycap.getCapModel().getCreatedBy().toString());
 //	          }
-	
-	      var mycap = aa.cap.getCap(tmpLicObj).getOutput();
-	      if (tmpLicObj.valid && mycap.getCapModel().getCreatedByACA() == 'Y') {
-	          associatedLicensedProfessionalWithPublicUser(licObj.refLicModel.getLicSeqNbr(), mycap.getCapModel().getCreatedBy().toString());
-	          }
-	      licObj = licenseProfObject(stateLicense,LICENSETYPE);
-	      logDebug("161:Successfully created LP? " + licObj.valid);
+//	      licObj = licenseProfObject(stateLicense,LICENSETYPE);
+//	      logDebug("161:Successfully created LP? " + licObj.valid);
 	
 	   
 	      
