539ba2736b96403184917f18160ad0534a82ae99
NationalDev <nd.michigan@gmail.com>
2017-09-15 11:16:57 -0400

    licCapId

--- Scripts/Event/PRA;LICENSES!~!~!APPLICATION.js
+++ Scripts/Event/PRA;LICENSES!~!~!APPLICATION.js
@@ -364,7 +364,7 @@ var showMessage = true;
 	          licObj.refLicModel.setLicenseIssueDate(licCap.getFileDate());
 	          var expObj = null;
 	          var expDt = null;
-	          var expObjRes = aa.expiration.getLicensesByCapID(stateLicense);
+	          var expObjRes = aa.expiration.getLicensesByCapID(licCapId);
 	          if(expObjRes.getSuccess()) var expObj = expObjRes.getOutput();
 	          if (expObj != null) {
 	              expDt = aa.date.parseDate(expObj.getExpDateString());
