ec4ba769d7bc404d861cdfb741a184e6a2e22d93
Iman <sallami@detroitmi.gov>
2017-09-15 19:04:56 -0400

    Signed-off-by: Iman <sallami@detroitmi.gov>

--- Scripts/Event/PRA;LICENSES!~!~!APPLICATION.js
+++ Scripts/Event/PRA;LICENSES!~!~!APPLICATION.js
@@ -15,9 +15,22 @@ var showMessage = true;
 
 	if (isTaskStatus("License Issuance","Issued") && balanceDue <= 0) {
 	        
-		loadFees(capId);
-  
-		logDebug("License Fees: " + loadFees(capId));
+		
+		iCont = null;
+		feeArray = new Array();
+		feeArray = loadFees(capId);
+		if (feeArray.length > 0) {
+		    for (iCont in contactArray) {
+		            //branch("EMSE:SetContactRelationshipToContactTypeLoop");
+		            logDebug("Executing EMSE:SetContactRelationshipToContactTypeLoop");
+		            showDebug=3;
+		            tFee = feeArray[iCont];
+		            aa.print("code: " + tFee["code"] + " " + tFee["amount"] + " " + tFee["status"]);
+		            
+		    }
+		}	
+		
+		
 		
 //************************************ REPORT SELECTION **********************************
 
