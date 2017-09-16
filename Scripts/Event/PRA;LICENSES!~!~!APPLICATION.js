b521832294cbf071efcb8ea509616327d0c32f49
Iman <sallami@detroitmi.gov>
2017-09-15 20:15:17 -0400

    Signed-off-by: Iman <sallami@detroitmi.gov>

--- Scripts/Event/PRA;LICENSES!~!~!APPLICATION.js
+++ Scripts/Event/PRA;LICENSES!~!~!APPLICATION.js
@@ -39,9 +39,9 @@ var showMessage = true;
   
 //*********************************** STATIONARY ENGINEER  
   
-	var	LICENSETYPE = getAppSpecific("License Type", capId);
-  
-  	logDebug("License Type: " + LICENSETYPE);
+		var	LICENSETYPE = getAppSpecific("License Type", capId);
+		var myReport;
+			logDebug("License Type: " + LICENSETYPE);
   	
 if (LICENSETYPE = "1st Class Station Eng") {
   	     
@@ -51,10 +51,12 @@ if (LICENSETYPE = "1st Class Station Eng") {
   			addParameter(rParams,"TASK","Licenses Issuance");
   			addParameter(rParams,"ITEM NAME","LIC LICENSED PROFESSIONAL");
   			addParameter(rParams,"License Type","VALUE_");
-  			logDebug("Parameters: " + rParams);
+  			
 
    			runReport4EmailOrPrint(capId,"Stationary",null,rParams,null,null,"Licenses");
- 
+   			
+   			myReport="Stationary";
+   			logDebug("Parameters: " + rParams + "Report =" + myReport);
   			}
   	
 else if (LICENSETYPE = "2nd Class Station Eng")  {
@@ -64,11 +66,11 @@ else if (LICENSETYPE = "2nd Class Station Eng")  {
   			addParameter(rParams,"TASK","Licenses Issuance");
   			addParameter(rParams,"ITEM NAME","LIC LICENSED PROFESSIONAL");
   			addParameter(rParams,"License Type","VALUE_");
-  			
-  			logDebug("Parameters: " + rParams);
-			
+  	
   			runReport4EmailOrPrint(capId,"Stationary",null,rParams,null,null,"Licenses");
-
+  			
+   			myReport="Stationary";
+   			logDebug("Parameters: " + rParams + "Report =" + myReport);
 			} 
 
 else if (LICENSETYPE = "3rd Class Station Eng") {
@@ -79,9 +81,10 @@ else if (LICENSETYPE = "3rd Class Station Eng") {
 			addParameter(rParams,"ITEM NAME","LIC LICENSED PROFESSIONAL");
 			addParameter(rParams,"License Type","VALUE_");
 			
-			logDebug("Parameters: " + rParams);
-        
 			runReport4EmailOrPrint(capId,"Stationary",null,rParams,null,null,"Licenses");
+			
+   			myReport="Stationary";
+   			logDebug("Parameters: " + rParams + "Report =" + myReport);
 
 		} 
 
@@ -97,10 +100,11 @@ else if (LICENSETYPE = "Boiler Op HP") {
   			addParameter(rParams,"ITEM NAME","LIC LICENSED PROFESSIONAL");
   			addParameter(rParams,"TASK","Licenses Issuance");
   			addParameter(rParams,"License Type","VALUE_");
-  			
-  			logDebug("Parameters: " + rParams);
-	    	        
+  			  			   	        
   			runReport4EmailOrPrint(capId,"Boiler",null,rParams,null,null,"Licenses");
+  			
+   			myReport="Boiler";
+   			logDebug("Parameters: " + rParams + "Report =" + myReport);
 
   	}
 
@@ -113,9 +117,10 @@ else if (LICENSETYPE = "Boiler Op LP") {
   			addParameter(rParams,"TASK","Licenses Issuance");
   			addParameter(rParams,"License Type","VALUE_");
   			
-  			logDebug("Parameters: " + rParams);
-		    	        
-			runReport4EmailOrPrint(capId,"Boiler",null,rParams,null,null,"Licenses");
+  			runReport4EmailOrPrint(capId,"Boiler",null,rParams,null,null,"Licenses");
+  			
+  			myReport="Boiler";
+   			logDebug("Parameters: " + rParams + "Report =" + myReport);
 
 	}
  
@@ -127,10 +132,11 @@ else if (LICENSETYPE = "1st Class Refrig Op") {
   			addParameter(rParams,"ITEM NAME","LIC LICENSED PROFESSIONAL");
   			addParameter(rParams,"TASK","Licenses Issuance");
   			addParameter(rParams,"License Type","VALUE_");
-  			
-  			logDebug("Parameters: " + rParams);
-    	        
+  			    	        
 			runReport4EmailOrPrint(capId,"Boiler",null,rParams,null,null,"Licenses");
+			
+			myReport="Boiler";
+   			logDebug("Parameters: " + rParams + "Report =" + myReport);
 
 	}
  
@@ -142,10 +148,10 @@ else  if (LICENSETYPE = "2nd Class Refrig Op") {
 			addParameter(rParams,"TASK","Licenses Issuance");
 			addParameter(rParams,"License Type","VALUE_");
 			
-			logDebug("Parameters: " + rParams);
-
-    	        
 			runReport4EmailOrPrint(capId,"Boiler",null,rParams,null,null,"Licenses");
+			
+			myReport="Boiler";
+   			logDebug("Parameters: " + rParams + "Report =" + myReport);
 
 	}
  
@@ -157,10 +163,11 @@ else if (LICENSETYPE = "3rd Class Refrig Op") {
   			addParameter(rParams,"ITEM NAME","LIC LICENSED PROFESSIONAL");
   			addParameter(rParams,"TASK","Licenses Issuance");
   			addParameter(rParams,"License Type","VALUE_");
-  			
-  			logDebug("Parameters: " + rParams);
-    	        
+     	        
 			runReport4EmailOrPrint(capId,"Boiler",null,rParams,null,null,"Licenses");
+			
+			myReport="Boiler";
+   			logDebug("Parameters: " + rParams + "Report =" + myReport);
 
 	}
 //*********************************** ALL OTHERS *************************************************************
@@ -174,13 +181,15 @@ else {
 		addParameter(rParams,"TASK","Licenses Issuance");
 		addParameter(rParams,"ITEM NAME","LIC LICENSED PROFESSIONAL");
 		addParameter(rParams,"License Type","VALUE_");
+  	    	        
+		runReport4EmailOrPrint(capId,"License",null,rParams,null,null,"Licenses");
 		
-
-		logDebug("Parameters: " + rParams);
-   	    	        
-  			runReport4EmailOrPrint(capId,"License",null,rParams,null,null,"Licenses");
+		myReport="License";
+		logDebug("Parameters: " + rParams + "Report =" + myReport);
   			
 }
 
+myReport="Boiler";
+	logDebug("Parameters: " + rParams + "Report =" + stop);
 	}
 	
