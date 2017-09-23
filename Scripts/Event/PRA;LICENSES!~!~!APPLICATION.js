8966cda361b5a1d5c9a2eefd816f185f9548db0d
Iman <sallami@detroitmi.gov>
2017-09-23 07:59:58 -0400

    no try-catch
    Signed-off-by: Iman <sallami@detroitmi.gov>

--- Scripts/Event/PRA;LICENSES!~!~!APPLICATION.js
+++ Scripts/Event/PRA;LICENSES!~!~!APPLICATION.js
@@ -527,12 +527,7 @@ var showMessage = true;
   			
   	    
   	    	
- }catch (err) {
-		logDebug("A JavaScript Error occured: " + err.message + " In Line " + err.lineNumber);
-	}
-// end user code
-	aa.env.setValue("ScriptReturnCode", "1"); 	aa.env.setValue("ScriptReturnMessage", debug)
-  
-    		}	
+
+    		
     
 	}
