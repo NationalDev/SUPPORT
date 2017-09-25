d9bab98413be089d6c75bda1aae35fe061b603b2
NationalDev <nd.michigan@gmail.com>
2017-09-25 10:52:03 -0400

    "Licenses"

--- Scripts/Event/PRA;LICENSES!~!~!APPLICATION.js
+++ Scripts/Event/PRA;LICENSES!~!~!APPLICATION.js
@@ -432,7 +432,8 @@ var showMessage = true;
 		        	  			addParameter(rParams,"TASK","Licenses Issuance");
 		        	  			addParameter(rParams,"License Type","VALUE_");
 		        	  			
-		        	  			runReport4EmailOrPrint(capId,"Stationary",null,rParams,null,null,"Licenses");
+//		        	  			runReport4EmailOrPrint(capId,"Stationary",null,rParams,null,null,"Licenses");
+		        	  			runReportAndSendAsync("Stationary", "Licenses", capId, rParams, null, null, null, null, null);
 		        	   			
 		        	   			logDebug("Parameters: " + rParams + "Report = " + myReport);        	  			 
 		        	  		   	}
@@ -447,7 +448,8 @@ var showMessage = true;
 		        	  			addParameter(rParams,"TASK","Licenses Issuance");
 		        	  			addParameter(rParams,"License Type","VALUE_");
 		        	  			
-		        	  			runReport4EmailOrPrint(capId,"Stationary",null,rParams,null,null,"Licenses");
+//		        	  			runReport4EmailOrPrint(capId,"Licenses",null,rParams,null,null,"Licenses");
+		        	  			runReportAndSendAsync("Boiler", "Licenses", capId, rParams, null, null, null, null, null);
 		        	   			
 		        	   			logDebug("Parameters: " + rParams + "Report = " + myReport);
 		        	  			}
