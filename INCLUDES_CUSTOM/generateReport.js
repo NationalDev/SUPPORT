c5bb38204dfecabc3ea4cdae19006ac7d8d6517e
Iman <sallami@detroitmi.gov>
2017-09-27 13:04:37 -0400

    Signed-off-by: Iman <sallami@detroitmi.gov>

--- INCLUDES_CUSTOM/generateReport.js
+++ INCLUDES_CUSTOM/generateReport.js
@@ -8,34 +8,34 @@
  * @returns reportFile
  */
 
-function generateReport(aaReportName, parameters, rModule) {
-	
-	var reportName = aaReportName;
-	
-	report = aa.reportManager.getReportInfoModelByName(reportName);
-	report = report.getOutput();
-	report.setModule(rModule);
-	report.setCapId(capId);
-	report.setReportParamaeters(parameters);
-	
-	var permit = aa.reportManager.hasPermission(reportName, currentUserID);
-	
-	if (permit.getOutput().booleanValue()) {
-		
-		var reportResult = aa.reportManager.getReportResult(report);
-		
-		if (reportResult) {
-			reportResult = reportResult.getOutput();
-			var reportFile = aa.reportManager.storeReportToDisk(reportResult);
-			logMessage("Report Result: " + reportResult);
-			reportFile = reportFile.getOutput();
-			return reportFile;
-		} else {
-			logMessage("Unable to run report: " + reportName + " For Admin "+ systemUserObj);
-			return false;
-		}
-	} else {
-		logMessage("Unable to run report: " + reportName + " For Admin "+ systemUserObj);
-		return false;
-	}
+function generateReport(itemCap, reportName, module, parameters) {
+    //returns the report file which can be attached to an email.
+    var user = currentUserID;   // Setting the User Name
+    var report = aa.reportManager.getReportInfoModelByName(reportName);
+    report = report.getOutput();
+    report.setModule(module);
+    report.setCapId(itemCap);
+    report.setReportParameters(parameters);
+    var vAltId = itemCap.getCustomID();
+    report.getEDMSEntityIdModel().setAltId(vAltId);
+
+    var permit = aa.reportManager.hasPermission(reportName, user);
+    if (permit.getOutput().booleanValue()) {
+        var reportResult = aa.reportManager.getReportResult(report);
+        if (!reportResult.getSuccess()) {
+            logDebug("System failed get report: " + reportResult.getErrorType() + ":" + reportResult.getErrorMessage());
+            return false;
+        }
+        else {
+            var reportOutput = reportResult.getOutput();
+            var reportFile = aa.reportManager.storeReportToDisk(reportOutput);
+            reportFile = reportFile.getOutput();
+            logDebug("Report " + reportName + " generated for record " + itemCap.getCustomID());
+            return reportFile;
+        }
+    }
+    else {
+        logDebug("Permissions are not set for report " + reportName + ".");
+        return false;
+    }
 }
