9c1a7da36cac8d5116e4af337c90bcadce186f3c
Iman <sallami@detroitmi.gov>
2017-09-28 10:10:50 -0400

    Stationary
    Signed-off-by: Iman <sallami@detroitmi.gov>

--- Scripts/Event/PRA;LICENSES!~!~!APPLICATION.js
+++ Scripts/Event/PRA;LICENSES!~!~!APPLICATION.js
@@ -443,7 +443,7 @@ var showMessage = true;
 										
 						if ((LICENSETYPE == "1st Class Station Eng") || (LICENSETYPE == "2nd Class Station Eng") || (LICENSETYPE == "3rd Class Station Eng")) {
 							 
-							myReport="Staionary";
+							myReport="Stationary";
 							
 							}
 
