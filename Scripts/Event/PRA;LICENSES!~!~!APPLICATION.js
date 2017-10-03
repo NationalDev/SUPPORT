235eb13cc5285430470a8fad833a34b04a61dac0
Iman <sallami@detroitmi.gov>
2017-10-03 12:00:57 -0400

    Signed-off-by: Iman <sallami@detroitmi.gov>

--- Scripts/Event/PRA;LICENSES!~!~!APPLICATION.js
+++ Scripts/Event/PRA;LICENSES!~!~!APPLICATION.js
@@ -560,18 +560,18 @@ var showMessage = true;
 		aa.env.setValue("ScriptReturnCode", "1"); 	aa.env.setValue("ScriptReturnMessage", debug)
 		
 		
-		function runReportAsync(reportName,module,itemCap,reportParameters) {
-			var scriptName = "RUNREPORTASYNC";
-
-			var envParameters = aa.util.newHashMap();
-			envParameters.put("ReportName",reportName);
-			envParameters.put("ReportParameters",reportParameters);
-			envParameters.put("Module",module);
-			envParameters.put("CustomCapId",capIDString);
-			envParameters.put("ReportUser",currentUserID);
-			envParameters.put("ServProvCode",servProvCode);
-			envParameters.put("ErrorEmailTo","saxthelm@accela.com");
-			envParameters.put("DebugEmailTo","saxthelm@accela.com");
-
-			aa.runAsyncScript(scriptName, envParameters);
-		}
+//		function runReportAsync(reportName,module,itemCap,reportParameters) {
+//			var scriptName = "RUNREPORTASYNC";
+//
+//			var envParameters = aa.util.newHashMap();
+//			envParameters.put("ReportName",reportName);
+//			envParameters.put("ReportParameters",reportParameters);
+//			envParameters.put("Module",module);
+//			envParameters.put("CustomCapId",capIDString);
+//			envParameters.put("ReportUser",currentUserID);
+//			envParameters.put("ServProvCode",servProvCode);
+//			envParameters.put("ErrorEmailTo","saxthelm@accela.com");
+//			envParameters.put("DebugEmailTo","saxthelm@accela.com");
+//
+//			aa.runAsyncScript(scriptName, envParameters);
+//		}
