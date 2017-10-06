4c4f67e5fc7d7b27e6aa91b4cbb0c2c776def45b
Iman <sallami@detroitmi.gov>
2017-10-06 09:34:32 -0400

    n
    Signed-off-by: Iman <sallami@detroitmi.gov>

--- Scripts/Event/PRA;LICENSES!~!~!APPLICATION.js
+++ Scripts/Event/PRA;LICENSES!~!~!APPLICATION.js
@@ -362,7 +362,7 @@ try{
 //-----------------------------------------------------------------------------------
 			        licObj.refLicModel.setLicenseIssueDate(sysDate);
 			        
-			        licObj.refLicModel.setExpiration(dateAdd(newExpDate,0));
+			        licObj.refLicModel.setExpiration(dateAdd(newExpDate));
 //_______________________________________________________________________________________			        
 			        var expObj = null;
 			        var expDt = null;
