127b5686d3d9744f7adaa875502153a5f256c43b
Iman <sallami@detroitmi.gov>
2017-10-06 12:20:43 -0400

    Signed-off-by: Iman <sallami@detroitmi.gov>

--- Scripts/Event/PRA;LICENSES!~!~!APPLICATION.js
+++ Scripts/Event/PRA;LICENSES!~!~!APPLICATION.js
@@ -22,20 +22,20 @@ try{
 				newLic = null;
 				newLicIdString = null;
 				monthsToInitialExpire = 12;
-				newLicId = parentCapId;
+				newLicId = null;
 			
-					logDebug(" newLicId = " + newLicId +" parent = " + parentCapId);
+					logDebug(" newLicId1 = " + newLicId +" parent = " + parentCapId);
 				
 				if (newLicId==null){
 									
 					newLicId = createParent(appTypeArray[0], appTypeArray[1], appTypeArray[2], "License",null);
 				}
 								
-					logDebug(" newLicId = " + newLicId +" parent = " + parentCapId);
+					logDebug(" newLicId2 = " + newLicId +" parent = " + parentCapId);
 					
 					newLicIdString = newLicId.getCustomID();
 	  
-					logDebug(" newLicId = " + newLicId +" parent = " + parentCapId);
+					logDebug(" newLicId3 = " + newLicId +" parent = " + parentCapId);
 		
         
 				        copyAppSpecific(capId);
