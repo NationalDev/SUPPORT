b1e731c7b6342a9c701080de232993ea9718cedf
Nickie Albert <nalbert@accela.com>
2017-10-06 13:16:34 -0400

    Initial - test

---
+++ Scripts/Event/MSA;CODEENFORCEMENT!INSPECTIONS!DANGEROUSBUILDINGS!COMPLAINT.js
@@ -0,0 +1,9 @@
+// MSA:CodeEnforcement/Inspections/DangerousBuildings/Complaint
+
+logDebug("CalendarID: " + CalendarID);
+logDebug("MeetingID: " + MeetingID);
+
+var mtgInfo = aa.meeting.getMeetingByMeetingID(CalendarID ,MeetingID );
+var meetingDate = mtgInfo.output.getStartDate();
+logDebug("The Meeting date is:" + meetingDate);
+
