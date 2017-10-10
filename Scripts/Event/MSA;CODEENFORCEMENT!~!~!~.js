ca827f8df9f3eb1c17dd10e6c1610bf16f5f20cc
Nickie Albert <nalbert@accela.com>
2017-10-10 11:46:19 -0400

    Added lookup to Variable branch

--- Scripts/Event/MSA;CODEENFORCEMENT!~!~!~.js
+++ Scripts/Event/MSA;CODEENFORCEMENT!~!~!~.js
@@ -1,6 +1,10 @@
 // MSA:CODEENFORCEMENT/*/*/*
 
 // showDebug = true;
+
+var prefix = lookup("EMSE_VARIABLE_BRANCH_PREFIX", "MeetingScheduleAfter");
+logDebug(prefix);
+
 logDebug("In event script MSA:CODEENFORCEMENT/*/*/*");
 logDebug("CalendarID: " + CalendarID);
 logDebug("MeetingID: " + MeetingID);
@@ -10,4 +14,3 @@ var meetingType = mtgInfo.getMeetingType();
 var meetingDate = mtgInfo.getStartDate();
 logDebug("Meeting Type: " + meetingType);
 logDebug("Meeting Date: " + meetingDate);
-
