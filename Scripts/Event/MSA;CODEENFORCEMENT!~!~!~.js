095aefcc89cc4f0b51ee71d049a89ff3f1e82dbd
Nickie Albert <nalbert@accela.com>
2017-10-11 14:16:35 -0400

    Added vars

--- Scripts/Event/MSA;CODEENFORCEMENT!~!~!~.js
+++ Scripts/Event/MSA;CODEENFORCEMENT!~!~!~.js
@@ -9,5 +9,11 @@ logDebug("MeetingID: " + MeetingID);
 var mtgInfo = aa.meeting.getMeetingByMeetingID(CalendarID, MeetingID).getOutput()
 var meetingType = mtgInfo.getMeetingType();
 var meetingDate = mtgInfo.getStartDate();
+var meetingTime = mtgInfo.getStartTime();
+var meetingLocation = mtgInfo.getMeetingLocation();
+
 logDebug("Meeting Type: " + meetingType);
 logDebug("Meeting Date: " + meetingDate);
+logDebug("Meeting Time: " + meetingTime);
+logDebug("Meeting location: " + meetingLocation);
+
